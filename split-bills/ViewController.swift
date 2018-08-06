780975cdd78c1cfdf1f95eeb9e9517fd38b0887f
Miguel de Elias <miguel.deelias@gmail.com>
2018-08-06 19:11:26 +1000

    Initial Commit

---
+++ split-bills/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  split-bills
+//
+//  Created by Carlos Miguel de Elias on 6/8/18.
+//  Copyright © 2018 Carlos Miguel de Elias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
