05ed81ae555f0cd7550f642ff10a8fc035c9dd8f
Maikol <miguel.deelias@gmail.com>
2020-04-23 22:27:30 +1000

    Testing CI

--- fastlane/swift/Gymfile.swift
+++ fastlane/swift/Gymfile.swift
@@ -9,9 +9,8 @@
 //
 
 class Gymfile: GymfileProtocol {
-// If you want to enable `gym`, run `fastlane gym init`
-// After, this file will be replaced with a custom implementation that contains values you supplied
-// during the `init` process, and you won't see this message
+
+    var scheme: { return "split-bills" }
 }
 
 
